0b1e3d68a149c5bbbbdd9edc6daf4b2f4882ac06
Nizar Daffa Maulana <izardev@gmail.com>
2025-02-24 12:14:18 +0700

    Add challenge.js Pertemuan 1

---
+++ 01_Pengantar_Praktikum_KPL/challenge.js
@@ -0,0 +1,29 @@
+import process from "process";
+import readline from "readline";
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let data = [];
+
+input.question("Siapa nama anda? ", (nama) => {
+    data.push(nama);
+
+    input.question("Apa jurusan anda? ", (jurusan) => {
+        data.push(jurusan);
+    
+        input.question("Apa universitas anda? ", (universitas) => {
+            data.push(universitas);
+        
+            input.question("Berapa usia anda? ", (usia) => {
+                data.push(usia);
+            
+                console.log(`\nNama: ${data[0]}, \nJurusan: ${data[1]}, \nUniversitas: ${data[2]}, \nUsia: ${data[3]}`);
+            
+                input.close();
+            });
+        });
+    });
+});
--- 01_Pengantar_Praktikum_KPL/identital.js
+++ 01_Pengantar_Praktikum_KPL/identital.js
@@ -6,27 +6,8 @@ const input = readline.createInterface({
     output: process.stdout,
 });
 
-let data = [];
-
 input.question("Siapa nama anda? ", (nama) => {
-    // console.info(`Halo ${nama}.`);
-    data.push(nama);
+    console.info(`Halo ${nama}.`);
 
-    input.question("Apa jurusan anda? ", (jurusan) => {
-        data.push(jurusan);
-    
-        input.question("Apa universitas anda? ", (universitas) => {
-            data.push(universitas);
-        
-            input.question("Berapa usia anda? ", (usia) => {
-                data.push(usia);
-            
-                console.log(`\nNama: ${data[0]}, \nJurusan: ${data[1]}, \nUniversitas: ${data[2]}, \nUsia: ${data[3]}`);
-            
-                input.close();
-            });
-        });
-    });
+    input.close();
 });
-
-// nama, jurusan, univ, usia
