c6631536bbc6024909c80280f9cc783abc630540
Nizar Daffa Maulana <izardev@gmail.com>
2025-03-17 10:04:38 +0700

    Menambahkan implementasi dengan table-driven

---
+++ 04_OOP/TP_OOP_2311104019/index.js
@@ -0,0 +1,7 @@
+// KodePos
+const KodePos = require("./KodePos")
+
+const kodePos = new KodePos()
+console.log("Kode Pos Batununggal:", kodePos.getKodePos("Batununggal"))
+console.log("Kode Pos Maleer:", kodePos.getKodePos("Maleer"))
+console.log("Kode Pos Kaligondang:", kodePos.getKodePos("Kaligondang"))
