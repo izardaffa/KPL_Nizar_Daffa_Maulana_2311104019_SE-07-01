8ccc3ac1e2a7d18a6ffb42a0a7d19c06f7802738
Nizar Daffa Maulana <izardev@gmail.com>
2025-03-17 10:06:27 +0700

    Menambahkan implementasi dengan state-based contruction

---
+++ 04_OOP/TP_OOP_2311104019/DoorMachine.js
@@ -0,0 +1,21 @@
+class DoorMachine {
+    constructor() {
+        this.state = "Terkunci";
+    }
+
+    unlock() {
+        if (this.state === "Terkunci") {
+            this.state = "Terbuka";
+            console.log("Pintu tidak terkunci");
+        }
+    }
+
+    lock() {
+        if (this.state === "Terbuka") {
+            this.state = "Terkunci";
+            console.log("Pintu terkunci");
+        }
+    }
+}
+
+module.exports = DoorMachine;
--- 04_OOP/TP_OOP_2311104019/index.js
+++ 04_OOP/TP_OOP_2311104019/index.js
@@ -5,3 +5,9 @@ const kodePos = new KodePos()
 console.log("Kode Pos Batununggal:", kodePos.getKodePos("Batununggal"))
 console.log("Kode Pos Maleer:", kodePos.getKodePos("Maleer"))
 console.log("Kode Pos Kaligondang:", kodePos.getKodePos("Kaligondang"))
+
+// DoorMachine
+const door = new DoorMachine()
+door.lock()
+door.unlock()
+door.lock()
